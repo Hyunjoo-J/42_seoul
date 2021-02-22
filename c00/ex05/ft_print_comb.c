43dd917cfff1a2d2bda39fe06c3b94fe3544bacf
Hyunjoo Jeong <hyjeong@c10r4s12.42seoul.kr>
2021-02-23 01:08:13 +0900

    42

---
+++ c00/ex05/ft_print_comb.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyjeong <hyjeong@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/22 18:04:41 by hyjeong           #+#    #+#             */
+/*   Updated: 2021/02/22 18:15:37 by hyjeong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+
+	a = '0' - 1;
+	while (a++ < '7')
+	{
+		b = a;
+		while (b++ < '8')
+		{
+			c = b;
+			while (c++ < '9')
+			{
+				if (!(a == '0' && b == '1' && c == '2'))
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				ft_putchar(a);
+				ft_putchar(b);
+				ft_putchar(c);
+			}
+		}
+	}
+}
---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyjeong <hyjeong@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/22 20:18:59 by hyjeong           #+#    #+#             */
+/*   Updated: 2021/02/22 20:45:04 by hyjeong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_num(int a, int b)
+{
+	ft_putchar('0' + a / 10);
+	ft_putchar('0' + a % 10);
+	ft_putchar(' ');
+	ft_putchar('0' + b / 10);
+	ft_putchar('0' + b % 10);
+	ft_putchar(',');
+	ft_putchar(' ');
+}
+
+void	ft_print_comb2(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (a < 98)
+	{
+		b = a + 1;
+		print_num(a, b);
+		a++;
+	}
+	write(1, "98 99", 5);
+}
